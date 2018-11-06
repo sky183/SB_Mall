065356a14f87acb104e64df5c799142ca5cde484
pc <pc@pc-PC>
2018-11-06 12:59:54 +0900

    .

---
+++ SB_Mall/src/main/java/com/sb/mall/login/controller/MemberController.java
@@ -0,0 +1,91 @@
+package com.sb.mall.login.controller;
+
+import java.sql.SQLException;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sb.mall.member.service.MemberService;
+
+@Controller // 클라이언의 요청을 처리 한 뒤그 결과를 DispatcherServlet에게 알려 줌 Struts의 Action과 동일한 기능
+public class MemberController {
+
+	@Autowired // Spring Framework에서 지원하는 의존주입 용도의 어노테이션
+	private MemberService loginService;
+
+	// 요청에 대해 어떤 Controller, 어떤 메소드가 처리할지를 맵핑하기 위한 어노테이션
+	@RequestMapping(value = "member/login", method = RequestMethod.GET)
+	// 쿠키 값 저장
+	public ModelAndView getLoginForm(@CookieValue(value = "idcookie", required = false) String rememberId) {
+		// ModelAndView 객체 생성
+		ModelAndView modelAndView = new ModelAndView();
+		// loginForm으로 보내줌
+		modelAndView.setViewName("/login/loginForm");
+		modelAndView.addObject("idcookie", rememberId);
+
+		return modelAndView;
+
+	}
+
+	@RequestMapping(value = "/member/login", method = RequestMethod.POST)
+	// id,pw ,session,cookie 값 받아온다
+	public ModelAndView loginProcess(@RequestParam(value = "userId", required = false) String userId,
+			@RequestParam(value = "userPw", required = false) String userPw,
+			@RequestParam(value = "rememberId", required = false) String rememberId, HttpSession session,
+			HttpServletResponse response
+
+	) throws SQLException {
+
+		System.out.println(rememberId);
+		ModelAndView modelAndView = new ModelAndView();
+
+		// 아이디저장 버튼이 on일 경우 쿠키생성
+		if ("on".equals(rememberId)) {
+			response.addCookie(new Cookie("idcookie", userId));
+
+		}
+
+
+		if (userId != null && userPw != null) {
+// userId 또는 userPw가 null 이 아닌 경우 
+			if (loginService.login(userId, userPw, session)) {
+				modelAndView.setViewName("redirect:/");
+			} else {
+				modelAndView.setViewName("login/loginForm");
+				modelAndView.addObject("error", "아이디 또는 비밀번호가 틀렸습니다.");
+			}
+
+		}
+
+		return modelAndView;
+	}
+
+	
+	
+	@RequestMapping("/error/loginError")
+	public String loginError() {
+		return "/error/loginError";
+	}
+
+	@RequestMapping("/member/logout") // 요청한 주소로 들어온다
+	public String logout(HttpSession session) {
+
+		// 세션의 종료
+		session.invalidate();
+
+		return "redirect:/";
+
+	}
+	
+	
+}
+
