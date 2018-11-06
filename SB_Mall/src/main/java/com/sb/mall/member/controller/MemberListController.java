d7c0034479884825ec919bbe694fa140ff84b4bf
chriskim <chriskim@DESKTOP-29LC9DT>
2018-11-06 13:12:24 +0900

    .

--- SB_Mall/src/main/java/com/sb/mall/member/controller/MemberListController.java
+++ SB_Mall/src/main/java/com/sb/mall/member/controller/MemberListController.java
@@ -1,4 +1,4 @@
-package com.sb.mall.controller;
+package com.sb.mall.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- SB_Mall/src/main/java/com/sb/mall/member/service/MemberLoginService.java
+++ SB_Mall/src/main/java/com/sb/mall/member/service/MemberLoginService.java
@@ -8,11 +8,11 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.sb.mall.dao.MemberDao;
-import com.sb.mall.model.MemberInfo;
+import com.sb.mall.member.dao.MemberDao;
+import com.sb.mall.member.model.MemberInfo;
 
 @Repository
-public class MemberService {
+public class MemberLoginService {
 
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
