83e3f4b708b05046756af36086a9225a1b33a737
David <gksekdnlt2@naver.com>
2018-12-20 21:06:28 +0900

    오더컨트롤러,서비스 - 다윗

--- SB_Mall/src/main/java/com/sb/mall/member/controller/MemberOrderListController.java
+++ SB_Mall/src/main/java/com/sb/mall/member/controller/MemberOrderListController.java
@@ -1,4 +1,4 @@
-package com.sb.mall.member.service;
+package com.sb.mall.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sb.mall.home.model.PageListView;
+import com.sb.mall.member.service.MemberOrderListService;
 
 @Controller
 public class MemberOrderListController {
@@ -17,14 +18,12 @@ public class MemberOrderListController {
 	@RequestMapping(value="/member/orderList",method=RequestMethod.GET)
 	public ModelAndView getOrderList() {
 		ModelAndView modelAndView = new ModelAndView();
-		PageListView list;
 		return modelAndView;
 	}
 	
 	@RequestMapping(value="/member/orderDetailList",method=RequestMethod.GET)
 	public ModelAndView getOrderDetailList() {
 		ModelAndView modelAndView = new ModelAndView();
-		PageListView list;
 		return modelAndView;
 	}
 
